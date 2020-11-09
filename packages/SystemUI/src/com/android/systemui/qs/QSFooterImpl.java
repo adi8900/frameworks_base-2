go-find-a-job/platform_frameworks_base

base: Add a toggle for QS footer text
Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

packages/SystemUI/src/com/android/systemui/qs/QSFooterImpl.java
@@ -42,7 +42,6 @@ import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.annotation.Nullable;
 import androidx.annotation.VisibleForTesting;
 
@@ -50,7 +49,6 @@ import com.android.internal.logging.MetricsLogger;
 import com.android.internal.logging.nano.MetricsProto;
 import com.android.keyguard.KeyguardUpdateMonitor;
 import com.android.settingslib.Utils;
-import com.android.settingslib.development.DevelopmentSettingsEnabler;
 import com.android.settingslib.drawable.UserIconDrawable;
 import com.android.systemui.Dependency;
 import com.android.systemui.R;
@@ -101,7 +99,7 @@ public class QSFooterImpl extends FrameLayout implements QSFooter,
 
     private OnClickListener mExpandClickListener;
 
-    private final ContentObserver mDeveloperSettingsObserver = new ContentObserver(
+    private final ContentObserver mSettingsObserver = new ContentObserver(
             new Handler(mContext.getMainLooper())) {
         @Override
         public void onChange(boolean selfChange, Uri uri) {
@@ -165,7 +163,10 @@ public class QSFooterImpl extends FrameLayout implements QSFooter,
     private void setBuildText() {
         TextView v = findViewById(R.id.build);
         if (v == null) return;
-        if (DevelopmentSettingsEnabler.isDevelopmentSettingsEnabled(mContext)) {
+        boolean isShow = Settings.System.getIntForUser(mContext.getContentResolver(),
+                        Settings.System.DERP_FOOTER_TEXT_SHOW, 0,
+                        UserHandle.USER_CURRENT) == 1;
+        if (isShow) {
             v.setText("#DerpFest");
             v.setVisibility(View.VISIBLE);
         } else {
@@ -251,15 +252,15 @@ public class QSFooterImpl extends FrameLayout implements QSFooter,
     protected void onAttachedToWindow() {
         super.onAttachedToWindow();
         mContext.getContentResolver().registerContentObserver(
-                Settings.Global.getUriFor(Settings.Global.DEVELOPMENT_SETTINGS_ENABLED), false,
-                mDeveloperSettingsObserver, UserHandle.USER_ALL);
+                Settings.System.getUriFor(Settings.System.DERP_FOOTER_TEXT_SHOW), false,
+                mSettingsObserver, UserHandle.USER_ALL);
     }
 
     @Override
     @VisibleForTesting
     public void onDetachedFromWindow() {
         setListening(false);
-        mContext.getContentResolver().unregisterContentObserver(mDeveloperSettingsObserver);
+        mContext.getContentResolver().unregisterContentObserver(mSettingsObserver);
         super.onDetachedFromWindow();
     }
 

packages/SystemUI/src/com/android/systemui/qs/customize/QSCustomizer.java
@@ -134,8 +134,13 @@ public class QSCustomizer extends LinearLayout implements OnMenuItemClickListene
         int qsTitlesValue = Settings.System.getIntForUser(mContext.getContentResolver(),
                 Settings.System.OMNI_QS_TILE_TITLE_VISIBILITY, 1,
                 UserHandle.USER_CURRENT);
+        int qsFooterValue = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.DERP_FOOTER_TEXT_SHOW, 0,
+                UserHandle.USER_CURRENT);
         MenuItem qsTitlesMenuItem = mToolbar.getMenu().findItem(R.id.menu_item_titles);
+        MenuItem qsFooterText = mToolbar.getMenu().findItem(R.id.footer_text);
         qsTitlesMenuItem.setChecked(qsTitlesValue == 1);
+        qsFooterText.setChecked(qsFooterValue == 1);
 
         mToolbar.setTitleTextColor(accentColor);
         mToolbar.getNavigationIcon().setTint(accentColor);
@@ -329,6 +334,11 @@ public class QSCustomizer extends LinearLayout implements OnMenuItemClickListene
             item.setChecked(!item.isChecked());
             Settings.System.putIntForUser(mContext.getContentResolver(),
                    Settings.System.OMNI_QS_TILE_TITLE_VISIBILITY, item.isChecked() ? 1 : 0,
+                   UserHandle.USER_CURRENT);
+        } else if (id == R.id.footer_text) {
+            item.setChecked(!item.isChecked());
+            Settings.System.putIntForUser(mContext.getContentResolver(),
+                   Settings.System.DERP_FOOTER_TEXT_SHOW, item.isChecked() ? 1 : 0,
                    UserHandle.USER_CURRENT);
         } else if (id == R.id.menu_item_rows_one) {
             Settings.System.putIntForUser(mContext.getContentResolver(),

adi8900/frameworks_base-2

Change-Id: If765a1de25141de568d0dc7b8f1be1f1308fd4e6

packages/SystemUI/src/com/android/systemui/qs/QSFooterImpl.java
@@ -42,7 +42,6 @@ import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.annotation.Nullable;
 import androidx.annotation.VisibleForTesting;
 
@@ -50,7 +49,6 @@ import com.android.internal.logging.MetricsLogger;
 import com.android.internal.logging.nano.MetricsProto;
 import com.android.keyguard.KeyguardUpdateMonitor;
 import com.android.settingslib.Utils;
-import com.android.settingslib.development.DevelopmentSettingsEnabler;
 import com.android.settingslib.drawable.UserIconDrawable;
 import com.android.systemui.Dependency;
 import com.android.systemui.R;
@@ -101,7 +99,7 @@ public class QSFooterImpl extends FrameLayout implements QSFooter,
 
     private OnClickListener mExpandClickListener;
 
-    private final ContentObserver mDeveloperSettingsObserver = new ContentObserver(
+    private final ContentObserver mSettingsObserver = new ContentObserver(
             new Handler(mContext.getMainLooper())) {
         @Override
         public void onChange(boolean selfChange, Uri uri) {
@@ -164,11 +162,11 @@ public class QSFooterImpl extends FrameLayout implements QSFooter,
     private void setBuildText() {
         TextView v = findViewById(R.id.build);
         if (v == null) return;
-        if (DevelopmentSettingsEnabler.isDevelopmentSettingsEnabled(mContext)) {
-            v.setText(mContext.getString(
-                    com.android.internal.R.string.bugreport_status,
-                    Build.VERSION.RELEASE_OR_CODENAME,
-                    Build.ID));
+        boolean isShow = Settings.System.getIntForUser(mContext.getContentResolver(),
+                        Settings.System.QS_FOOTER_TEXT_SHOW, 0,
+                        UserHandle.USER_CURRENT) == 1;
+        if (isShow) {
+            v.setText("#StayDerped");
             v.setVisibility(View.VISIBLE);
         } else {
             v.setVisibility(View.GONE);
@@ -253,15 +251,15 @@ public class QSFooterImpl extends FrameLayout implements QSFooter,
     protected void onAttachedToWindow() {
         super.onAttachedToWindow();
         mContext.getContentResolver().registerContentObserver(
-                Settings.Global.getUriFor(Settings.Global.DEVELOPMENT_SETTINGS_ENABLED), false,
-                mDeveloperSettingsObserver, UserHandle.USER_ALL);
+                Settings.System.getUriFor(Settings.System.QS_FOOTER_TEXT_SHOW), false,
+                mSettingsObserver, UserHandle.USER_ALL);
     }
 
     @Override
     @VisibleForTesting
     public void onDetachedFromWindow() {
         setListening(false);
-        mContext.getContentResolver().unregisterContentObserver(mDeveloperSettingsObserver);
+        mContext.getContentResolver().unregisterContentObserver(mSettingsObserver);
         super.onDetachedFromWindow();
     }
 

packages/SystemUI/src/com/android/systemui/qs/customize/QSCustomizer.java
@@ -145,8 +145,13 @@ public class QSCustomizer extends LinearLayout implements OnMenuItemClickListene
         int qsTitlesValue = Settings.System.getIntForUser(mContext.getContentResolver(),
                 Settings.System.OMNI_QS_TILE_TITLE_VISIBILITY, 1,
                 UserHandle.USER_CURRENT);
+        int qsFooterValue = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.DERP_FOOTER_TEXT_SHOW, 0,
+                UserHandle.USER_CURRENT);
         MenuItem qsTitlesMenuItem = mToolbar.getMenu().findItem(R.id.menu_item_titles);
+        MenuItem qsFooterText = mToolbar.getMenu().findItem(R.id.footer_text);
         qsTitlesMenuItem.setChecked(qsTitlesValue == 1);
+        qsFooterText.setChecked(qsFooterValue == 1);
 
         mToolbar.setTitleTextColor(accentColor);
         mToolbar.getNavigationIcon().setTint(accentColor);
@@ -368,6 +373,11 @@ public class QSCustomizer extends LinearLayout implements OnMenuItemClickListene
             item.setChecked(!item.isChecked());
             Settings.System.putIntForUser(mContext.getContentResolver(),
                    Settings.System.OMNI_QS_TILE_TITLE_VISIBILITY, item.isChecked() ? 1 : 0,
+                   UserHandle.USER_CURRENT);
+        } else if (id == R.id.footer_text) {
+            item.setChecked(!item.isChecked());
+            Settings.System.putIntForUser(mContext.getContentResolver(),
+                   Settings.System.QS_FOOTER_TEXT_SHOW, item.isChecked() ? 1 : 0,
                    UserHandle.USER_CURRENT);
         }
 